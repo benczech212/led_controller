858b708aaadb5a75c0ff68a68dbccfb3ff785438
Ben <ben@shadow-caster.com>
2020-01-03 19:26:31 -0500

    More work on inheritence of the button class

--- button controller/code.py
+++ button controller/code.py
@@ -84,45 +84,70 @@ printh = printHelper()
 
 
 ######################################################################
-class button:
-    def __init__(self, name, position, device, color):
-        defaultColor = (128,128,128)
-        defaultPressedColor = (255,255,255)
-        if color == None:
-            for index, groupName in enumerate(buttonColorDefaults):
-                if str(groupName) == str(group):
-                    color = effects.wheel(buttonColorDefaults[groupName])
-                else:  color = defaultColor
+class buttonColor:
+    def __init__(self, shownColor,notShownColor,selectedColor,highlightedColor,velocity):
+        
+        self.shownColor = shownColor
+        self.notShownColor = notShownColor
+        self.selectedColor = selectedColor
+        self.highlightedColor = highlightedColor
+        self.last = [0,0,0]
+        self.current = [0,0,0]
+        self.target = [0,0,0]
+        self.delta = [0,0,0]
+        self.nextStep = [0,0,0]
+        self.velocity = velocity
+        self.setColorTarget(shownColor)
+    
+    def setColorTarget(self,colorTarget):
+        self.target = colorTarget
+        self.update()
+
+    def update(self):
+        for i in range(len(self.current)):
+            self.delta[i] = (self.target[i]-self.current[i])
             
-        self.name = name
-        self.group = group
-        self.position = position
-        self.posX = self.position[0]
-        self.posY = self.position[1]
-        self.pressed = False
-        self.defaultColor = color
-        self.colorLast = color
-        self.currentColor = color
-        self.colorPressed = defaultPressedColor
-        self.lit = True
-        self.shown = False
+        for i in range(len(self.current)):
+            if abs(self.target[i] - self.current[i]) < self.velocity:
+                # Less than 1 step away 
+                self.nextStep[i] = self.target[i]
+            elif (self.target[i] > self.current[i]):
+                # Fade Up 1 Velocity
+                self.nextStep[i] = self.current[i] + self.velocity
+            else:
+                # Fade Down 1 Velocity
+                self.nextStep[i] = self.current[i] - self.velocity
+
+
+
+
+class button:
+    def __init__(self, device, name, position):
         self.device = device
+        self.name = name
+        self.pos = position
+        self.posX = position[0]
+        self.posY = position[1]
+        self.pressed = False
+        self.lit = True
+        self.shown = True
+        self.color = buttonColor((255,255,0),(0,0,0),(32,32,32),(128,0,128),8)
         UIbuttons.append(self)
         
-    def update(self):
+    def update(self,buttonMatrix):
         for button in UIbuttons:
             if button.shown:
-                if button.lit:
-                    if button.pressed:
-                        button.device.pixels[button.posX,button.posY] = button.colorPressed
-                    else:
-                        button.device.pixels[button.posX,button.posY] = button.currentColor
+                button.color.update()
+                if button.pressed:
+                    button.color.target = button.color.selectedColor
                 else:
-                    button.device.pixels[button.posX,button.posY] = 0
+                    button.color.target = button.color.shownColor
+            else:
+                button.color.target = button.color.notShownColor
         trellis.pixels.show()
 
     def changeColor(self,color):
-        self.currentColor = color
+        self.color.current = color
 
     def changeColorBack(self):
         temp = self.currentColor
@@ -152,6 +177,8 @@ class button:
     def onPress(self):
         if self.shown:
             print("Button {} Down".format(self.name))    
+            self.color.target = self.color.selectedColor
+            self.pressed = True
 
         
     def setUpColoredButtons(self,buttons):
@@ -164,7 +191,7 @@ class button:
 class buttonPanel:
     def __init__(self,name,buttons):
         self.name = name
-        self.shown = False
+        self.shown = True
         self.buttons = []
         self.buttonMatrix = buttonMatrix
         UIpanels.append(self)
@@ -177,9 +204,7 @@ class buttonPanel:
         else: self.showOff()
 
     def update(self):
-        if self.shown:
-            for button in self.buttons:
-                button.update()
+        pass
 
     def showOff(self):
         self.shown = False
@@ -196,11 +221,13 @@ class buttonPanel:
         if self.shown:
             self.showOn()
         else: self.showOff()
+    def onPress(self):
+        pass
 
 
 ######################################################################
 class buttonMatrix:
-    def __init__(self, device, name, xmin, ymin, xmax, ymax):
+    def __init__(self, device, name, xmin, ymin, xmax, ymax, buttonPannels):
         self.device = device
         self.name = name
         self.xmin = xmin
@@ -214,28 +241,44 @@ class buttonMatrix:
         self.lastPressed = set()
         self.lastReleased = set()
         self.currentPress = set()
-
+        self.buttonPanels = buttonPannels
         
-    def updatePressed(self):
+
+    def update(self):
+        self.checkForInputs()
+        self.updatePanels()
+        self.updateButtons()
+                
+    def updatePanels(self):
+        for panel in self.buttonPanels:
+            panel.update()
+
+    def updateButtons(self):
+        for button in UIbuttons:
+            button.update(self)
+
+    def checkForInputs(self):
         try:
             self.keysPressed = set(self.device.pressed_keys)
         except:
             print("Error Getting Key Presses")
-    
-    def checkForInputs(self):
-        self.updatePressed()
+            raise
         #Only Most Recent Press
         for press in self.keysPressed - self.currentPress:
             self.lastPressed = press
-            onAnyPress(press)
-                
-        
+            self.onPress(press)
         for release in self.currentPress - self.keysPressed:
             if release:
                 onAnyRelease(release)
-        self.currentPress = self.keysPressed
 
-    
+    def onPress(self,press):
+        for panel in UIpanels:
+            if panel.shown:
+                panel.onPress()
+                for button in panel.buttons:
+                    if button.shown:
+                        button.onPress()
+
         
 
     def index2xy(self,pixelIndex):
@@ -248,52 +291,20 @@ class buttonMatrix:
         return (y * self.xrange) + x
 
 
-
-matrix = buttonMatrix(trellis,"neotrellis",0,0,7,3,)
 brightnessButtons = [
-    button(('Brightness Min'),'brightness',(4,0),trellis,(0,0,0)),
-    button(('Brightness Minus'),'brightness',(5,0),trellis,(32,32,32)),
-    button(('Brightness Plus'),'brightness',(6,0),trellis,(192,192,192)),
-    button(('Brightness Max'),'brightness',(7,0),trellis,(255,255,255)),
-    ]
-colorButtons = [
-    button(('Colored Button'),'color',(5,1),trellis,None),
-    button(('Colored Button'),'color',(6,1),trellis,None),
-    button(('Colored Button'),'color',(7,1),trellis,None),
-    button(('Colored Button'),'color',(5,2),trellis,None),
-    button(('Colored Button'),'color',(6,2),trellis,None),
-    button(('Colored Button'),'color',(7,2),trellis,None),
-    button(('Colored Button'),'color',(5,3),trellis,None),
-    button(('Colored Button'),'color',(6,3),trellis,None),
-    button(('Colored Button'),'color',(7,3),trellis,None),
-    ]
-menuButtons = [
-    button(('menu 1'),'menu',(0,0),trellis,(194,128,0)),
-    button(('menu 2'),'menu',(1,0),trellis,(194,194,0)),
-    button(('menu 3'),'menu',(2,0),trellis,(194,194,0)),
-    button(('menu 4'),'menu',(3,0),trellis,(194,194,0)),
-    ]
-color1 = effects.wheel(10)
-color2 = effects.wheel(40)
-color3 = effects.wheel(80)
-cursorButtons = [
-    button(('Cursor UL'),'cursorArrowAlt',(0,1),trellis,color1),
-    button(('Cursor CL'),'cursorArrow',(0,2),trellis,color2),
-    button(('Cursor DL'),'cursorArrowAlt',(0,3),trellis,color1),
-    button(('Cursor UC'),'cursorArrow',(1,1),trellis,color2),
-    button(('Cursor DC'),'cursorArrow',(1,3),trellis,color2),
-    button(('Cursor UR'),'cursorArrowAlt',(2,1),trellis,color1),
-    button(('Cursor CR'),'cursorArrow',(2,2),trellis,color2),
-    button(('Cursor DR'),'cursorArrowAlt',(2,3),trellis,color1),
-    button(('Cursor CC'),'cursorCenter',(1,2),trellis,color3),
+    button(trellis,'Brightness Min',(4,0)),
+    button(trellis,'Brightness Min',(5,0)),
+    #button(trellis,'Brightness Min',(6,0)),
+    #button(trellis,'Brightness Min',(7,0)),
 ]
-channelButtons = [
-    button(('R Channel'),'channel',(3,1),trellis,(255,0,0)),
-    button(('G Channel'),'channel',(3,2),trellis,(0,255,0)),
-    button(('B Channel'),'channel',(3,3),trellis,(0,0,255)),
+brightnessPanel = buttonPanel("brightness",brightnessButtons)
+matrix = buttonMatrix(trellis,"neotrellis",0,0,7,3,[brightnessPanel])
 
-]
-colorButtons[0].setUpColoredButtons(colorButtons)
+    #button(trellis,('Brightness Minus'),(5,0),buttonColor(,
+    #button(trellis,('Brightness Plus'),(6,0),buttonColor((255,255,0),(0,0,0),(196,196,196),(128,0,128),8)),
+    #button(trellis,('Brightness Max'),(7,0),buttonColor((255,255,0),(0,0,0),(255,255,255),(128,0,128),8)),
+    
+
 
 
 
@@ -360,35 +371,26 @@ for button in UIbuttons:
     button.showOn()
     print(button.shown)
 
+
 #for colorButtons
-colorPanel = buttonPanel("colorPanel",colorButtons)
-brightnessPanel = buttonPanel("brightnessPanel",brightnessButtons)
-channelPanel = buttonPanel("channelPanel",channelButtons)
-cursorPanel = buttonPanel("cursorPanel",cursorButtons)
-cursorArrowPanel = buttonPanel("cursorArrowPanel",None)
-for button in cursorButtons:
-    if button.group == "cursorArrow":
-        cursorArrowPanel.addToPanel(button)
-cursorArrowAltPanel = buttonPanel("cursorArrowAltPanel",None)
-for button in cursorButtons:
-    if button.group == "cursorArrowAlt":
-        cursorArrowAltPanel.addToPanel(button)
-cursorButtonPanel = buttonPanel("cursorButton",None)
-for button in cursorButtons:
-    if button.group == "cursorButton":
-        cursorButtonPanel.addToPanel(button)
+#colorPanel = buttonPanel("colorPanel",colorButtons)
+#brightnessPanel = buttonPanel("brightnessPanel",brightnessButtons)
+#channelPanel = buttonPanel("channelPanel",channelButtons)
+#cursorPanel = buttonPanel("cursorPanel",cursorButtons)
+#cursorArrowPanel = buttonPanel("cursorArrowPanel",None)
+#menuPanel = buttonPanel("menuPanel",menuButtons)
 
-menuPanel = buttonPanel("menuPanel",buttonMatrix)
-
-colorPanel.showOff()    
-brightnessPanel.showOff()
-channelPanel.showOff()
-for panel in UIpanels:
-    print(panel.name,panel.shown)
+#colorPanel.showOff()    
+#brightnessPanel.showOff()
+#channelPanel.showOff()
+#for panel in UIpanels:
+    #print(panel.name,panel.shown)
 
 
 while True:
+    matrix.update()        
     
+        
     
     
 
